9b5b8125a7a47dc9d25f00632c30dcf7ea47c608
David Benjamin Lugo Avendaño <davidbenjaminlugo@gmail.com>
2026-04-01 19:08:42 -0300

    Create Primer_programa.cs

---
+++ Primer_programa.cs
@@ -0,0 +1,12 @@
+namespace primer_programa;
+
+class Program
+{
+    static void Main(string[] args)
+    {//inicia mi programa
+        //aqui dentro se codeo
+        Console.WriteLine("Hola David!");
+        Console.WriteLine("Ahora aprenderemos en C#");
+        Console.WriteLine("lol");
+    }//fin de mi programa
+}
